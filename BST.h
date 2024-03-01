e7b1a096dd66102f997332453474239a6bf2486d
Trigggerwarning <156408826+Trigggerwarning@users.noreply.github.com>
2024-03-01 10:03:16 +0000

    moooo

--- BST.h
+++ BST.h
@@ -16,24 +16,34 @@ typedef struct {
 // prototypes
 
 void insertNode( BST *, int );
-/*
+void displayBST(TreeNodePtr ,int);
+
+
 void insertNode_R(TreeNodePtr * t,int value){
-  if(t==NULL){
-   t=new TreeNode;
-	 if(t){
-		t->leftPtr=NULL;
-		t->rightPtr=NULL;
-		t->data=value;
-   }
+  if(*t==NULL){
+      *t = (TreeNodePtr)malloc(sizeof(struct TreeNode));	 
+      if(*t){
+		   (*t)->leftPtr=NULL;
+		   (*t)->rightPtr=NULL;
+		   (*t)->data=value;
+      }
+      else{
+         printf("Error: Memory allocation failed\n");
+         return;
+      }
   }
   else{
-     if(t->data>=value) //left
+     if((*t)->data>=value){
+       insertNode_R(&((*t)->leftPtr), value);
+     } //left
         
-     else //right
+     else if((*t)->data<value){
+      insertNode_R(&((*t)->rightPtr), value);
+     } //right
       
    }
 }
-*/
+
 void insertNode( BST *b, int value ){
 	 TreeNodePtr t =b->root,new_node;
 	 int inserted=0;
@@ -43,20 +53,34 @@ void insertNode( BST *b, int value ){
 		new_node->rightPtr=NULL;
 		new_node->data=value;
 		/*First Node*/
-		if(!b->root)	b->root=new_node;
+		if(!b->root) b->root=new_node;
 		else {
-while(!inserted){
-   if(t->data >=value){
-     /* move/insert to the left*/
-    
-     }
+         while(!inserted){
+            if(t->data >=value){
+               /* move/insert to the left*/
+               if(!t->leftPtr){
+                  t->leftPtr=new_node;
+                  inserted=1;
+               }
+               else{
+               t=t->leftPtr;
+
+               }
+            }
   	 
-  }
-   else{
-	      /* move/ insert to the right*/
-    
+  
+            else{
+	            /* move/ insert to the right*/
+               if(!t->rightPtr){
+                  t->rightPtr=new_node;
+                  inserted=1;
+               }
+            else{
+         t=t->rightPtr;
+
+      }
     }
-	}
+	
    
   }//end while		
   }//end else;
@@ -73,8 +97,43 @@ void inOrder( TreeNodePtr treePtr )
         
       inOrder( treePtr->leftPtr ); //Recursion to the left
  
-      printf("%3d",treePtr->data) ;  //print the value 
+      printf("%3d ",treePtr->data) ;  //print the value 
    
       inOrder( treePtr->rightPtr ); //Recursion to the right
    } // end if                          
 } // end 
+
+void preOrder(TreeNodePtr treePtr) {
+    if (treePtr != NULL) {
+        printf("%3d ", treePtr->data);
+        preOrder(treePtr->leftPtr);
+        preOrder(treePtr->rightPtr);
+    }
+}
+
+void postOrder(TreeNodePtr treePtr) {
+    if (treePtr != NULL) {
+        postOrder(treePtr->leftPtr);
+        postOrder(treePtr->rightPtr);
+        printf("%3d ", treePtr->data);
+    }
+}
+void displayBST(TreeNodePtr root,int x) {
+   int i;
+    if (root != NULL) {
+      x=x+1;
+
+
+         // Recursively display the right subtree
+        displayBST(root->rightPtr,x);
+        for(i=0;i<x*3;i++)printf(" ");
+         // Print the value of the current node
+        printf("%3d \n", root->data);
+        // Recursively display the left subtree
+        displayBST(root->leftPtr,x);
+
+        
+
+        
+    }
+}
